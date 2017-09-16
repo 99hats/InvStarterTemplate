fb85e9bd1110e1da329ff37521c92a9c13bbda1c
Kevin Smith <kevin@mcweekly.com>
2017-09-16 14:07:32 -0700

    working draft ;=)

--- InvDefault/Class1.cs
+++ InvDefault/Class1.cs
@@ -1,25 +1,80 @@
 ﻿using Inv;
+using System.Collections.Generic;
 
 namespace InvDefault
 {
-    public static class Shell
+  public static class Shell
+  {
+    public static void Install(Inv.Application Application)
     {
-        public static void Install(Inv.Application Application)
+      Application.Title = "My Project";
+
+      var Surface = Application.Window.NewSurface();
+      Surface.Background.Colour = Colour.WhiteSmoke;
+
+      var pageDock = Surface.NewVerticalDock();
+      
+
+      var tabsDock = Surface.NewHorizontalDock();
+      tabsDock.Alignment.CenterStretch();
+      tabsDock.Background.Colour = Colour.Black;
+
+      var horizontalScroll = Surface.NewHorizontalScroll();
+      horizontalScroll.Content = tabsDock;
+
+      
+
+      pageDock.AddClient( horizontalScroll);
+      var tabTexts = new List<string> { "Recent Posts", "Current Issue", "Events Calendar" };
+
+      Label current = null;
+
+      for (var index = 0; index < tabTexts.Count; index++)
+      {
+
+        var sep = Surface.NewFrame();
+        sep.Border.Set(1, 0, 0, 0);
+        sep.Margin.Set(0, 4);
+        sep.Border.Colour = Colour.Gray;
+
+        var tabText = tabTexts[index];
+        var tabLabel = Surface.NewLabel();
+        tabLabel.Text = tabText;
+        tabLabel.Font.Colour = Colour.White;
+        tabLabel.Font.Size = 24;
+        tabLabel.Padding.Set(16, 4);
+       
+        if (index ==0 )
         {
-            Application.Title = "My Project";
-
-            var Surface = Application.Window.NewSurface();
-            Surface.Background.Colour = Colour.WhiteSmoke;
-            var Label = Surface.NewLabel();
-            Surface.Content = Label;
-            Label.Alignment.Center();
-            Label.Font.Size = 20;
-            Label.Text = $"Howdy";
-
-            var graphic = Surface.NewGraphic();
-            //graphic.Image = Inv.Default.Resources.Images.Logo;
-
-            Application.Window.Transition(Surface);
+          tabLabel.Border.Colour = Colour.Red;
+          tabLabel.Border.Set(0, 0, 0, 3);
+          current = tabLabel;
         }
+
+        var tabButton = Surface.NewButton();
+        tabButton.SingleTapEvent += () =>
+        {
+          var thisLabel = tabButton.Content as Label;
+          thisLabel.Border.Colour = Colour.Red;
+          tabLabel.Border.Set(0,0,0,3);
+          current.Border.Set(0);
+          current = thisLabel;
+        };
+        tabButton.Content = tabLabel;
+
+        tabsDock.AddHeader(tabButton);
+        if (index < tabTexts.Count) tabsDock.AddHeader(sep);
+
+      }
+
+
+
+
+
+      Surface.Content = pageDock;
+      Application.Window.Transition(Surface);
     }
+  }
+
+
 }
