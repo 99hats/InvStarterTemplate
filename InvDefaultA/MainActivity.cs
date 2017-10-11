bc6d2e255bd6265f3614f8a117a054984435c7b5
99hats <ksmith@99hats.com>
2017-10-10 22:28:37 -0700

    Otherwise Droid crashes

--- InvDefaultA/MainActivity.cs
+++ InvDefaultA/MainActivity.cs
@@ -5,7 +5,7 @@ using Application = Inv.Application;
 
 namespace InvDefaultA
 {
-    [Activity(Label = "InvDefaultA", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "InvDefaultA", MainLauncher = true, Icon = "@drawable/icon",  ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     public class MainActivity : Inv.AndroidActivity
     {
         public override void Install(Application application)
